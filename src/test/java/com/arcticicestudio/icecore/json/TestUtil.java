39c301ca9a17086e957c2c9c10703223d2715a95
Arctic Ice Studio <development@arcticicestudio.com>
2016-05-28 20:18:17 +0200

    Implement test class "TestUtil"
    Provides test utils.

---
+++ src/test/java/com/arcticicestudio/icecore/json/TestUtil.java
@@ -0,0 +1,41 @@
+/*
++++++++++++++++++++++++++++++++++++++++++++
+title     Test Util                       +
+project   icecore-json                    +
+file      TestUtil.java                   +
+version                                   +
+author    Arctic Ice Studio               +
+email     development@arcticicestudio.com +
+website   http://arcticicestudio.com      +
+copyright Copyright (C) 2016              +
+created   2016-05-28 20:00 UTC+0200       +
+modified  2016-05-28 20:01 UTC+0200       +
++++++++++++++++++++++++++++++++++++++++++++
+
+[Description]
+Provides test utils.
+
+[Copyright]
+Copyright (C) 2016 Arctic Ice Studio <development@arcticicestudio.com>
+
+[References]
+JSON
+  (http://json.org)
+ECMA-404 1st Edition (October 2013)
+  (http://www.ecma-international.org/publications/files/ECMA-ST/ECMA-404.pdf)
+Java 8 API Documentation
+  (https://docs.oracle.com/javase/8/docs/api/)
+JUnit
+  (http://junit.org)
+Arctic Versioning Specification (ArcVer)
+  (http://specs.arcticicestudio.com/arcver)
+*/
+package com.arcticicestudio.icecore.json;
+
+/**
+ * Provides test utils.
+ *
+ * @author Arctic Ice Studio &lt;development@arcticicestudio.com&gt;
+ * @since 0.7.0
+ */
+public class TestUtil {}
