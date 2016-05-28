0f787ab85a29059425d81420b7f2e08319972589
Arctic Ice Studio <development@arcticicestudio.com>
2016-05-28 16:07:54 +0200

    Create main API class "Json"
    Serves as the entry point to the "IceCore - JSON" public API.

---
+++ src/main/java/com/arcticicestudio/icecore/json/Json.java
@@ -0,0 +1,61 @@
+/*
+++++++++++++++++++++++++++++++++++++++++++++
+title     JSON Public API                  +
+project   icecore-json                     +
+file      Json.java                        +
+version                                    +
+author    Arctic Ice Studio                +
+email     development@arcticicestudio.com  +
+website   http://arcticicestudio.com       +
+copyright Copyright (C) 2016               +
+created   2016-05-28 16:06 UTC+0200        +
+modified  2016-05-28 16:07 UTC+0200        +
+++++++++++++++++++++++++++++++++++++++++++++
+
+[Description]
+Serves as the entry point to the "IceCore - JSON" public API.
+
+[Copyright]
+Copyright (C) 2016 Arctic Ice Studio <development@arcticicestudio.com>
+
+[References]
+JSON
+  (http://json.org)
+ECMA-404 1st Edition (October 2013)
+  (http://www.ecma-international.org/publications/files/ECMA-ST/ECMA-404.pdf)
+Java 8 API Documentation
+  (https://docs.oracle.com/javase/8/docs/api/)
+Arctic Versioning Specification (ArcVer)
+  (http://specs.arcticicestudio.com/arcver)
+*/
+package com.arcticicestudio.icecore.json;
+
+/**
+ * This class serves as the entry point to the "IceCore - JSON" public API.
+ * <p>
+ *   To <strong>parse</strong> a given JSON input, use the {@code parse()} methods:
+ * </p>
+ * <pre>
+ * JsonObject object = Json.parse(string).asObject();
+ * </pre>
+ * <p>
+ *   To <strong>create</strong> a JSON data structure to be serialized, use the methods {@code value()},
+ *   {@code array()} and {@code object()}.
+ *   The following example snippet will produce the JSON string <em>{"foo": 23, "bar": true}</em>:
+ * </p>
+ * <pre>
+ * String string = Json.object().add("foo", 23).add("bar", true).toString();
+ * </pre>
+ * <p>
+ *   To create a JSON array from a given Java array, you can use one of the {@code array()} methods with varargs
+ *   parameters:
+ * </p>
+ * <pre>
+ * String[] names = ...
+ * JsonArray array = Json.array(names);
+ * </pre>
+ *
+ * @author Arctic Ice Studio &lt;development@arcticicestudio.com&gt;
+ * @since 0.6.0
+ */
+public final class Json {}
