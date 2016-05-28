4033ea27bc1cb8532e80e3ba3b16cd99a4d23e4e
Arctic Ice Studio <development@arcticicestudio.com>
2016-05-28 11:53:07 +0200

    Implement method "+ set(int,JsonValue):JsonArray"

--- src/main/java/com/arcticicestudio/icecore/json/JsonArray.java
+++ src/main/java/com/arcticicestudio/icecore/json/JsonArray.java
@@ -282,4 +282,20 @@ public class JsonArray extends JsonValue implements Iterable<JsonValue> {
     values.set(index, Json.value(value));
     return this;
   }
+
+  /**
+   * Replaces the element at the specified position in this array with the specified JSON value.
+   *
+   * @param index the index of the array element to replace
+   * @param value the value to be stored at the specified array position, must not be {@code null}
+   * @return the array itself, to enable method chaining
+   * @throws IndexOutOfBoundsException if the index is out of range ({@code index &lt; 0} or {@code index &gt;= size})
+   */
+  public JsonArray set(int index, JsonValue value) {
+    if (value == null) {
+      throw new NullPointerException("value is null");
+    }
+    values.set(index, value);
+    return this;
+  }
 }
