a6856674af9b69da66e70ef2a776d4870c95853b
Arctic Ice Studio <development@arcticicestudio.com>
2016-05-28 14:24:53 +0200

    Create class "JsonWriter"
    Writes the generated JSON object.

---
+++ src/main/java/com/arcticicestudio/icecore/json/JsonWriter.java
@@ -0,0 +1,42 @@
+/*
++++++++++++++++++++++++++++++++++++++++++++
+title     JSON Writer                     +
+project   icecore-json                    +
+file      JsonWriter.java                 +
+version                                   +
+author    Arctic Ice Studio               +
+email     development@arcticicestudio.com +
+website   http://arcticicestudio.com      +
+copyright Copyright (C) 2016              +
+created   2016-05-28 14:24 UTC+0200       +
+modified  2016-05-28 14:25 UTC+0200       +
++++++++++++++++++++++++++++++++++++++++++++
+
+[Description]
+Writes the generated JSON object.
+
+[Copyright]
+Copyright (C) 2016 Arctic Ice Studio <development@arcticicestudio.com>
+
+[References]
+JSON
+  (http://json.org)
+ECMA-404 1st Edition (October 2013)
+  (http://www.ecma-international.org/publications/files/ECMA-ST/ECMA-404.pdf)
+Java 8 API Documentation
+  (https://docs.oracle.com/javase/8/docs/api/)
+Arctic Versioning Specification (ArcVer)
+  (http://specs.arcticicestudio.com/arcver)
+StackOverflow
+  (http://stackoverflow.com/questions/2965293/javascript-parse-error-on-u2028-unicode-character)
+*/
+package com.arcticicestudio.icecore.json;
+
+/**
+ * Writes the generated JSON object.
+ *
+ * @author Arctic Ice Studio &lt;development@arcticicestudio.com&gt;
+ * @since 0.5.0
+ */
+
+class JsonWriter {}
