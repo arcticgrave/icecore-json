5c28432f921b748cdd03860db75df50937ab9af1
Arctic Ice Studio <development@arcticicestudio.com>
2016-05-28 13:50:35 +0200

    Create class "JsonParser" and implement class signature
    A JSON Parser.

---
+++ src/main/java/com/arcticicestudio/icecore/json/JsonParser.java
@@ -0,0 +1,39 @@
+/*
++++++++++++++++++++++++++++++++++++++++++++
+title     JSON Parser                     +
+project   icecore-json                    +
+file      JsonParser.java                 +
+version                                   +
+author    Arctic Ice Studio               +
+email     development@arcticicestudio.com +
+website   http://arcticicestudio.com      +
+copyright Copyright (C) 2016              +
+created   2016-05-28 13:49 UTC+0200       +
+modified  2016-05-28 13:50 UTC+0200       +
++++++++++++++++++++++++++++++++++++++++++++
+
+[Description]
+A JSON parser.
+
+[Copyright]
+Copyright (C) 2016 Arctic Ice Studio <development@arcticicestudio.com>
+
+[References]
+JSON
+  (http://json.org)
+ECMA-404 1st Edition (October 2013)
+  (http://www.ecma-international.org/publications/files/ECMA-ST/ECMA-404.pdf)
+Java 8 API Documentation
+  (https://docs.oracle.com/javase/8/docs/api/)
+Arctic Versioning Specification (ArcVer)
+  (http://specs.arcticicestudio.com/arcver)
+*/
+package com.arcticicestudio.icecore.json;
+
+/**
+ * A JSON parser.
+ *
+ * @author Arctic Ice Studio &lt;development@arcticicestudio.com&gt;
+ * @since 0.4.0
+ */
+class JsonParser {}
