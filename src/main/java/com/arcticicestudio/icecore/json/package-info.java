37534a9cf035485af0788ad2182647bf705a1865
Arctic Ice Studio <development@arcticicestudio.com>
2016-05-28 16:37:26 +0200

    Implement package info class for "com.arcticicestudio.icecore.json"

---
+++ src/main/java/com/arcticicestudio/icecore/json/package-info.java
@@ -0,0 +1,43 @@
+/*
++++++++++++++++++++++++++++++++++++++++++++
+title     Package Info                    +
+project   icecore-json                    +
+file      package-info.java               +
+version                                   +
+author    Arctic Ice Studio               +
+email     development@arcticicestudio.com +
+website   http://arcticicestudio.com      +
+copyright Copyright (C) 2016              +
+created   2016-0-28 16:27 UTC+0200        +
+modified  2016-0-28 16:36 UTC+0200        +
++++++++++++++++++++++++++++++++++++++++++++
+
+[Description]
+Represents the public API of the "IceCore - JSON" library.
+The main entry point of the package is the "Json" class to parse and write JSON.
+
+[Copyright]
+Copyright (C) 2016 Arctic Ice Studio <development@arcticicestudio.com>
+
+[References]
+JSON
+  (http://json.org)
+ECMA-404 1st Edition (October 2013)
+  (http://www.ecma-international.org/publications/files/ECMA-ST/ECMA-404.pdf)
+Java 8 API Documentation
+  (https://docs.oracle.com/javase/8/docs/api/)
+Arctic Versioning Specification (ArcVer)
+  (http://specs.arcticicestudio.com/arcver)
+*/
+/**
+ * Represents the public API of the <a href="https://bitbucket.com/arcticicestudio/icecore-json">"IceCore - JSON"</a>
+ * library.
+ * <p>
+ * <strong>The package represents the public API.</strong><br>
+ * The main entry point of the package is the {@link com.arcticicestudio.icecore.json.Json} class to parse and write
+ * JSON.
+ * </p>
+ *
+ * @author Arctic Ice Studio &lt;development@arcticicestudio.com&gt;
+ */
+package com.arcticicestudio.icecore.json;
