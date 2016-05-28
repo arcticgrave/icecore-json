2865dacbf5c6be2c42523bb795eac168f0ffe1a9
Arctic Ice Studio <development@arcticicestudio.com>
2016-05-28 14:34:07 +0200

    Implement protected method "writeArraySeparator():void"

--- src/main/java/com/arcticicestudio/icecore/json/JsonWriter.java
+++ src/main/java/com/arcticicestudio/icecore/json/JsonWriter.java
@@ -90,6 +90,10 @@ class JsonWriter {
     writer.write(']');
   }
 
+  protected void writeArraySeparator() throws IOException {
+    writer.write(',');
+  }
+
   protected void writeJsonString(String string) throws IOException {
     int length = string.length();
     int start = 0;
