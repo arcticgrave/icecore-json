13ebe3c5ba67e8bbb38183275221187cea96819c
Arctic Ice Studio <development@arcticicestudio.com>
2016-05-29 18:07:35 +0200

    Create test class "JsonParserTest"
    Tests the JSON parser class "JsonParser".

---
+++ src/test/java/com/arcticicestudio/icecore/json/JsonParserTest.java
@@ -0,0 +1,45 @@
+/*
++++++++++++++++++++++++++++++++++++++++++++
+title     JSON Parser Test                +
+project   icecore-json                    +
+file      JsonParserTest.java             +
+version                                   +
+author    Arctic Ice Studio               +
+email     development@arcticicestudio.com +
+website   http://arcticicestudio.com      +
+copyright Copyright (C) 2016              +
+created   2016-05-29 18:06 UTC+0200       +
+modified  2016-05-29 18:07 UTC+0200       +
++++++++++++++++++++++++++++++++++++++++++++
+
+[Description]
+Tests the JSON parser class "JsonParser".
+
+[Copyright]
+Copyright (C) 2016 Arctic Ice Studio <development@arcticicestudio.com>
+
+[References]
+JSON
+  (http://json.org)
+ECMA-404 1st Edition (October 2013)
+  (http://www.ecma-international.org/publications/files/ECMA-ST/ECMA-404.pdf)
+Java 8 API Documentation
+  (https://docs.oracle.com/javase/8/docs/api/)
+JUnit
+  (http://junit.org)
+Mockito
+  (http://mockito.org)
+Arctic Versioning Specification (ArcVer)
+  (http://specs.arcticicestudio.com/arcver)
+*/
+
+package com.arcticicestudio.icecore.json;
+
+/**
+ * Tests the JSON parser class {@link JsonParser}.
+ *
+ * @author Arctic Ice Studio &lt;development@arcticicestudio.com&gt;
+ * @since 0.7.0
+ */
+
+public class JsonParserTest {}
