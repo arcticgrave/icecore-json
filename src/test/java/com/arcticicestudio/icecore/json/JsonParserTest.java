e91a511d0340980a61b6ec96894be0b7f9616503
Arctic Ice Studio <development@arcticicestudio.com>
2016-05-29 18:26:16 +0200

    Implement test method "+ objectsMultipleValues():void"

--- src/test/java/com/arcticicestudio/icecore/json/JsonParserTest.java
+++ src/test/java/com/arcticicestudio/icecore/json/JsonParserTest.java
@@ -297,6 +297,11 @@ public class JsonParserTest {
     assertEquals("{\"yogurt\":92}", parse("{\"yogurt\":92}").toString());
   }
 
+  @Test
+  public void objectsMultipleValues() {
+    assertEquals("{\"yogurt\":92,\"coconut\":42}", parse("{\"yogurt\":92,\"coconut\":42}").toString());
+  }
+
   private static void assertParseException(int offset, String message, final String json) {
     ParseException exception = assertException(ParseException.class, new Runnable() {
       public void run() {
