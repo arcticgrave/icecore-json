a9f77b8900b4f53ccc7be1ac00b3c611ab755db8
Arctic Ice Studio <development@arcticicestudio.com>
2016-05-29 10:01:53 +0200

    Create test class "JsonArrayTest"
    Tests the JSON array structure representation class "JsonArray".

---
+++ src/test/java/com/arcticicestudio/icecore/json/JsonArrayTest.java
@@ -0,0 +1,41 @@
+/*
++++++++++++++++++++++++++++++++++++++++++++
+title     JSON Array Test                 +
+project   icecore-json                    +
+file      JsonArrayTest.java              +
+version                                   +
+author    Arctic Ice Studio               +
+email     development@arcticicestudio.com +
+website   http://arcticicestudio.com      +
+copyright Copyright (C) 2016              +
+created   2016-05-29 10:00 UTC+0200       +
+modified  2016-05-29 10:01 UTC+0200       +
++++++++++++++++++++++++++++++++++++++++++++
+
+[Description]
+Tests the JSON array structure representation class "JsonArray".
+
+[Copyright]
+Copyright (C) 2016 Arctic Ice Studio <development@arcticicestudio.com>
+
+[References]
+JSON
+  (http://json.org)
+ECMA-404 1st Edition (October 2013)
+  (http://www.ecma-international.org/publications/files/ECMA-ST/ECMA-404.pdf)
+Java 8 API Documentation
+  (https://docs.oracle.com/javase/8/docs/api/)
+JUnit
+  (http://junit.org)
+Arctic Versioning Specification (ArcVer)
+  (http://specs.arcticicestudio.com/arcver)
+*/
+package com.arcticicestudio.icecore.json;
+
+/**
+ * Tests the JSON array structure representation class {@link JsonArray}.
+ *
+ * @author Arctic Ice Studio &lt;development@arcticicestudio.com&gt;
+ * @since 0.7.0
+ */
+public class JsonArrayTest {}
