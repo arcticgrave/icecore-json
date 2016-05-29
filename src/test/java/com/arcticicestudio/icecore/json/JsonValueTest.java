25cb014dc7b25c5b6a1e4c776cf03b85d58ebd48
Arctic Ice Studio <development@arcticicestudio.com>
2016-05-29 09:46:53 +0200

    Implement test class "JsonValueTest"
    Tests the JSON values representation class "JsonValue".

---
+++ src/test/java/com/arcticicestudio/icecore/json/JsonValueTest.java
@@ -0,0 +1,42 @@
+/*
++++++++++++++++++++++++++++++++++++++++++++
+title     JSON Value Test                 +
+project   icecore-json                    +
+file      JsonValueTest.java              +
+version                                   +
+author    Arctic Ice Studio               +
+email     development@arcticicestudio.com +
+website   http://arcticicestudio.com      +
+copyright Copyright (C) 2016              +
+created   2016-05-29 09:44 UTC+0200       +
+modified  2016-05-29 09:45 UTC+0200       +
++++++++++++++++++++++++++++++++++++++++++++
+
+[Description]
+Tests the JSON values representation class "JsonValue".
+
+[Copyright]
+Copyright (C) 2016 Arctic Ice Studio <development@arcticicestudio.com>
+
+[References]
+JSON
+  (http://json.org)
+ECMA-404 1st Edition (October 2013)
+  (http://www.ecma-international.org/publications/files/ECMA-ST/ECMA-404.pdf)
+Java 8 API Documentation
+  (https://docs.oracle.com/javase/8/docs/api/)
+JUnit
+  (http://junit.org)
+Arctic Versioning Specification (ArcVer)
+  (http://specs.arcticicestudio.com/arcver)
+*/
+
+package com.arcticicestudio.icecore.json;
+
+/**
+ * Tests the JSON values representation class {@link JsonValue}.
+ *
+ * @author Arctic Ice Studio &lt;development@arcticicestudio.com&gt;
+ * @since 0.7.0
+ */
+public class JsonValueTest {}
