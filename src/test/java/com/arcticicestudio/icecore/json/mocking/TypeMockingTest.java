00ac6397bfa199ba50adf9926daca7cfada998e0
Arctic Ice Studio <development@arcticicestudio.com>
2016-05-29 21:37:23 +0200

    Create test class "TypeMockingTest"
    Tests mocking to make sure types do not prevent mocking
    by final or visibility constructs.

---
+++ src/test/java/com/arcticicestudio/icecore/json/mocking/TypeMockingTest.java
@@ -0,0 +1,43 @@
+/*
++++++++++++++++++++++++++++++++++++++++++++
+title     Type Mocking Test               +
+project   icecore-json                    +
+file      TypeMockingTest.java            +
+version                                   +
+author    Arctic Ice Studio               +
+email     development@arcticicestudio.com +
+website   http://arcticicestudio.com      +
+copyright Copyright (C) 2016              +
+created   2016-05-29 21:37 UTC+0200       +
+modified  2016-05-29 21:38 UTC+0200       +
++++++++++++++++++++++++++++++++++++++++++++
+
+[Description]
+Tests mocking to make sure types do not prevent mocking by final or visibility constructs.
+
+[Copyright]
+Copyright (C) 2016 Arctic Ice Studio <development@arcticicestudio.com>
+
+[References]
+JSON
+  (http://json.org)
+ECMA-404 1st Edition (October 2013)
+  (http://www.ecma-international.org/publications/files/ECMA-ST/ECMA-404.pdf)
+Java 8 API Documentation
+  (https://docs.oracle.com/javase/8/docs/api/)
+JUnit
+  (http://junit.org)
+Mockito
+  (http://mockito.org)
+Arctic Versioning Specification (ArcVer)
+  (http://specs.arcticicestudio.com/arcver)
+*/
+package com.arcticicestudio.icecore.json.mocking;
+
+/**
+ * Tests mocking to make sure types do not prevent mocking by final or visibility constructs.
+ *
+ * @author Arctic Ice Studio &lt;development@arcticicestudio.com&gt;
+ * @since 0.7.0
+ */
+public class TypeMockingTest {}
