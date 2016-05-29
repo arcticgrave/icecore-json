da7ae9a94524bae542cf7a44b0aae1b48825794b
Arctic Ice Studio <development@arcticicestudio.com>
2016-05-29 18:18:03 +0200

    Implement test method "+ parseIgnoresAllWhiteSpace():void"

--- src/test/java/com/arcticicestudio/icecore/json/JsonParserTest.java
+++ src/test/java/com/arcticicestudio/icecore/json/JsonParserTest.java
@@ -97,6 +97,11 @@ public class JsonParserTest {
     assertEquals(new JsonArray(), parse(" [ ] "));
   }
 
+  @Test
+  public void parseIgnoresAllWhiteSpace() {
+    assertEquals(new JsonArray(), parse("\t\r\n [\t\r\n ]\t\r\n "));
+  }
+
   private static void assertParseException(int offset, String message, final String json) {
     ParseException exception = assertException(ParseException.class, new Runnable() {
       public void run() {
