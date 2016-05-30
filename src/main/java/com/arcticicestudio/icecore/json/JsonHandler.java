0e265008c8c35dee7006281b209ff6ce58f62920
Arctic Ice Studio <development@arcticicestudio.com>
2016-05-30 22:47:26 +0200

    Implement method "+ startTrue():void"

--- src/main/java/com/arcticicestudio/icecore/json/JsonHandler.java
+++ src/main/java/com/arcticicestudio/icecore/json/JsonHandler.java
@@ -66,4 +66,10 @@ public abstract class JsonHandler<A, O> {
    * This method will be called when reading the last character of the literal.
    */
   public void endNull() {}
+
+  /**
+   * Indicates the beginning of a JSON {@code true} literal in the input.
+   * This method will be called when reading the first character of the literal.
+   */
+  public void startTrue() {}
 }
