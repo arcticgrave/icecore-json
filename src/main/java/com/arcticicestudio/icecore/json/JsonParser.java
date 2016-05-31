a1e56964a8b6ffbc9faab2e179cde9d51480683a
Arctic Ice Studio <development@arcticicestudio.com>
2016-05-31 22:35:15 +0200

    Fix offset off-by-one at end of input
    The read() method fails to increment the index when reading the last
    character of the input.
    Other methods compensate for this by treating the index differently
    when at end of text.
    By correctly incrementing the index also when at end of text, the
    conditional adjustments can be removed from endCapture() and
    getLocation().

--- src/main/java/com/arcticicestudio/icecore/json/JsonParser.java
+++ src/main/java/com/arcticicestudio/icecore/json/JsonParser.java
@@ -9,7 +9,7 @@ email     development@arcticicestudio.com +
 website   http://arcticicestudio.com      +
 copyright Copyright (C) 2016              +
 created   2016-05-28 13:49 UTC+0200       +
-modified  2016-05-30 22:29 UTC+0200       +
+modified  2016-05-31 22:35 UTC+0200       +
 +++++++++++++++++++++++++++++++++++++++++++
 
 [Description]
@@ -449,6 +449,7 @@ public class JsonParser {
       index = 0;
       if (fill == -1) {
         current = -1;
+        index++;
         return;
       }
     }
@@ -473,26 +474,24 @@ public class JsonParser {
   }
 
   private String endCapture() {
-    int end = current == -1 ? index : index - 1;
-    String captured;
-    if (captureBuffer.length() > 0) {
-      captureBuffer.append(buffer, captureStart, end - captureStart);
-      captured = captureBuffer.toString();
-      captureBuffer.setLength(0);
-    } else {
-      captured = new String(buffer, captureStart, end - captureStart);
-    }
+    int start = captureStart;
+    int end = index - 1;
     captureStart = -1;
-    return captured;
+    if (captureBuffer.length() > 0) {
+      captureBuffer.append(buffer, start, end - start);
+      String captured = captureBuffer.toString();
+      captureBuffer.setLength(0);
+      return captured;
+    }
+    return new String(buffer, start, end - start);
   }
 
   /**
    * @since 0.8.0
    */
   Location getLocation() {
-    int absIndex = bufferOffset + index;
-    int offset = isEndOfText() ? absIndex : absIndex - 1;
-    int column = absIndex - lineOffset - 1;
+    int offset = bufferOffset + index - 1;
+    int column = offset - lineOffset;
     return new Location(offset, column, line);
   }
 
