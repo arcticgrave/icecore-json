b28e4886c2e6ac6de57af626cbbf30ed9e5a622d
Arctic Ice Studio <development@arcticicestudio.com>
2016-08-12 14:28:10 +0200

    Remove unnecessary comment-documentation overhead

--- src/main/java/com/arcticicestudio/icecore/json/Location.java
+++ src/main/java/com/arcticicestudio/icecore/json/Location.java
@@ -8,25 +8,7 @@ author    Arctic Ice Studio               +
 email     development@arcticicestudio.com +
 website   http://arcticicestudio.com      +
 copyright Copyright (C) 2016              +
-created   2016-06-04 07:51 UTC+0200       +
-modified  2016-06-04 07:56 UTC+0200       +
 +++++++++++++++++++++++++++++++++++++++++++
-
-[Description]
-An immutable object that represents a location in the parsed text.
-
-[Copyright]
-Copyright (C) 2016 Arctic Ice Studio <development@arcticicestudio.com>
-
-[References]
-JSON
-  (http://json.org)
-ECMA-404 1st Edition (October 2013)
-  (http://www.ecma-international.org/publications/files/ECMA-ST/ECMA-404.pdf)
-Java 8 API Documentation
-  (https://docs.oracle.com/javase/8/docs/api/)
-Arctic Versioning Specification (ArcVer)
-  (http://specs.arcticicestudio.com/arcver)
 */
 package com.arcticicestudio.icecore.json;
 
--- src/test/java/com/arcticicestudio/icecore/json/PrettyPrintTest.java
+++ src/test/java/com/arcticicestudio/icecore/json/PrettyPrintTest.java
@@ -8,29 +8,7 @@ author    Arctic Ice Studio               +
 email     development@arcticicestudio.com +
 website   http://arcticicestudio.com      +
 copyright Copyright (C) 2016              +
-created   2016-05-29 21:03 UTC+0200       +
-modified  2016-05-29 21:14 UTC+0200       +
 +++++++++++++++++++++++++++++++++++++++++++
-
-[Description]
-Tests the JSON writer pretty print class "PrettyPrint".
-
-[Copyright]
-Copyright (C) 2016 Arctic Ice Studio <development@arcticicestudio.com>
-
-[References]
-JSON
-  (http://json.org)
-ECMA-404 1st Edition (October 2013)
-  (http://www.ecma-international.org/publications/files/ECMA-ST/ECMA-404.pdf)
-Java 8 API Documentation
-  (https://docs.oracle.com/javase/8/docs/api/)
-JUnit
-  (http://junit.org)
-Mockito
-  (http://mockito.org)
-Arctic Versioning Specification (ArcVer)
-  (http://specs.arcticicestudio.com/arcver)
 */
 package com.arcticicestudio.icecore.json;
 
