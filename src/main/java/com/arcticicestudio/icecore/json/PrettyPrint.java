f50bcb63f4afeac89c41b02fe10566cc33ba7acf
Arctic Ice Studio <development@arcticicestudio.com>
2016-08-12 14:18:41 +0200

    Remove unnecessary comment-documentation overhead

--- src/main/java/com/arcticicestudio/icecore/json/PrettyPrint.java
+++ src/main/java/com/arcticicestudio/icecore/json/PrettyPrint.java
@@ -8,25 +8,7 @@ author    Arctic Ice Studio               +
 email     development@arcticicestudio.com +
 website   http://arcticicestudio.com      +
 copyright Copyright (C) 2016              +
-created   2016-05-28 14:44 UTC+0200       +
-modified  2016-05-28 14:55 UTC+0200       +
 +++++++++++++++++++++++++++++++++++++++++++
-
-[Description]
-Enables human readable JSON output by inserting whitespace between values after commas and colons.
-
-[Copyright]
-Copyright (C) 2016 Arctic Ice Studio <development@arcticicestudio.com>
-
-[References]
-JSON
-  (http://json.org)
-ECMA-404 1st Edition (October 2013)
-  (http://www.ecma-international.org/publications/files/ECMA-ST/ECMA-404.pdf)
-Java 8 API Documentation
-  (https://docs.oracle.com/javase/8/docs/api/)
-Arctic Versioning Specification (ArcVer)
-  (http://specs.arcticicestudio.com/arcver)
 */
 
 package com.arcticicestudio.icecore.json;
